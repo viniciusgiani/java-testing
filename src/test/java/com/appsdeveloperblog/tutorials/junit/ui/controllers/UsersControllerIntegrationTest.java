760e3371cd186649aad287e30876e1651b3f831f
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2022-07-13 10:29:52 -0400

    @SpringBootTest + WebEnvironment.MOCK

---
+++ src/test/java/com/appsdeveloperblog/tutorials/junit/ui/controllers/UsersControllerIntegrationTest.java
@@ -0,0 +1,7 @@
+package com.appsdeveloperblog.tutorials.junit.ui.controllers;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+public class UsersControllerIntegrationTest {
+}
