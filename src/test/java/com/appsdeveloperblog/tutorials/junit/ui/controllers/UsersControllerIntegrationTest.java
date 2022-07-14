9802d8d85684fd33e202e595b24acfb399a6ae68
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2022-07-14 12:30:43 -0400

    @TestPropertySource annotation

--- src/test/java/com/appsdeveloperblog/tutorials/junit/ui/controllers/UsersControllerIntegrationTest.java
+++ src/test/java/com/appsdeveloperblog/tutorials/junit/ui/controllers/UsersControllerIntegrationTest.java
@@ -1,7 +1,21 @@
 package com.appsdeveloperblog.tutorials.junit.ui.controllers;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+//@TestPropertySource(locations = "/application-test.properties",
+//        properties = "server.port=8081")
+//@TestPropertySource(locations = "/application-test.properties")
 public class UsersControllerIntegrationTest {
+
+    @Value("${server.port}")
+    private int serverPort;
+
+    @Test
+    void contextLoads() {
+        System.out.println("server.port=" + serverPort);
+    }
 }
