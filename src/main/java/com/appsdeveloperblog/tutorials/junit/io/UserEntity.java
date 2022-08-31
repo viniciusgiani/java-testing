820d3bb91d898d02d4c3b8e005be14172008656c
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2022-08-31 10:16:46 -0400

    Updated UserEntity and UserRepository for demo test methods

--- src/main/java/com/appsdeveloperblog/tutorials/junit/io/UserEntity.java
+++ src/main/java/com/appsdeveloperblog/tutorials/junit/io/UserEntity.java
@@ -8,12 +8,11 @@ import java.io.Serializable;
 public class UserEntity implements Serializable {
 
     private static final long serialVersionUID = 5313493413859894403L;
-
     @Id
     @GeneratedValue
     private long id;
 
-    @Column(nullable=false)
+    @Column(nullable=false, unique=true)
     private String userId;
 
     @Column(nullable=false, length=50)
@@ -22,7 +21,7 @@ public class UserEntity implements Serializable {
     @Column(nullable=false, length=50)
     private String lastName;
 
-    @Column(nullable=false, length=120)
+    @Column(nullable=false, length=120, unique = true)
     private String email;
 
     @Column(nullable=false)
