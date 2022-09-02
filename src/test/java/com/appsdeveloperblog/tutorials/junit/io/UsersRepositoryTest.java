54c495dd0108f9f031d371a4571d4fa29cdb58b8
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2022-09-02 13:30:25 -0400

    Test findByUserId() query method

--- src/test/java/com/appsdeveloperblog/tutorials/junit/io/UsersRepositoryTest.java
+++ src/test/java/com/appsdeveloperblog/tutorials/junit/io/UsersRepositoryTest.java
@@ -1,6 +1,7 @@
 package com.appsdeveloperblog.tutorials.junit.io;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -12,27 +13,56 @@ import java.util.UUID;
 public class UsersRepositoryTest {
 
     @Autowired
-    TestEntityManager testEntityManager;
+    private TestEntityManager testEntityManager;
 
     @Autowired
-    UsersRepository usersRepository;
+    private UsersRepository usersRepository;
+
+    private final String userId1 = UUID.randomUUID().toString();
+    private final String userId2 = UUID.randomUUID().toString();
+    private final String email1 = "test@test.com";
+    private final String email2 = "test2@test.com";
+
+    @BeforeEach
+    void setup() {
+        // Creating first user
+        UserEntity userEntity = new UserEntity();
+        userEntity.setUserId(userId1);
+        userEntity.setEmail(email1);
+        userEntity.setEncryptedPassword("12345678");
+        userEntity.setFirstName("Sergey");
+        userEntity.setLastName("Kargopolov");
+        testEntityManager.persistAndFlush(userEntity);
+
+        // Creating second user
+        UserEntity userEntity2 = new UserEntity();
+        userEntity2.setUserId(userId2);
+        userEntity2.setEmail(email2);
+        userEntity2.setEncryptedPassword("abcdefg1");
+        userEntity2.setFirstName("John");
+        userEntity2.setLastName("Sears");
+        testEntityManager.persistAndFlush(userEntity2);
+    }
 
     @Test
     void testFindByEmail_whenGivenCorrectEmail_returnsUserEntity() {
-        // Arrange
-        UserEntity user = new UserEntity(); 
-        user.setFirstName("Sergey");
-        user.setLastName("Kargopolov");
-        user.setEmail("test@test.com");
-        user.setUserId(UUID.randomUUID().toString());
-        user.setEncryptedPassword("123456789");
-        testEntityManager.persistAndFlush(user);
-
         // Act
-        UserEntity storedUser = usersRepository.findByEmail(user.getEmail());
+        UserEntity storedUser = usersRepository.findByEmail(email1);
 
         // Assert
-        Assertions.assertEquals(user.getEmail(), storedUser.getEmail(),
+        Assertions.assertEquals(email1, storedUser.getEmail(),
                 "The returned email address does not match the expected value");
+    }
+
+    @Test
+    void testFindByUserId_whenGivenCorrectUserId_returnsUserEntity() {
+        // Act
+        UserEntity storedUser = usersRepository.findByUserId(userId2);
+
+        // Assert
+        Assertions.assertNotNull(storedUser,
+                "UserEntity object should not be null");
+        Assertions.assertEquals(userId2, storedUser.getUserId(),
+                "Returned userId does not much expected value");
     }
 }
